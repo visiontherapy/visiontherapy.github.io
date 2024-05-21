6ade51cb81e617252baa7a9e54992025a3de7d7f
quartertone <492588+quartertone@users.noreply.github.com>
2024-05-20 17:43:28 -0700

    shuffle function

---
+++ commonfn.js
@@ -0,0 +1,8 @@
+// COMMON FUNCTIONS
+
+function ashuffle(array) {
+  return array.map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+
+}
