3510f2240639ab6d9245b645176b8dadfec387ed
quartertone <492588+quartertone@users.noreply.github.com>
2024-07-05 16:35:03 -0700

    random with seed for hart chart

--- commonfn.js
+++ commonfn.js
@@ -6,3 +6,16 @@ function ashuffle(array) {
     .map(({ value }) => value);
 
 }
+
+
+
+
+function random(seed) {
+  let m = 2 ** 35 - 31;
+  let a = 185852;
+  let s = seed % m;
+  return function () {
+    return (s = (s * a) % m) / m;
+  };
+}
+
