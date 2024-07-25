8e8d8da6423e63b9e6f09964a8c82f5d5bea6adb
quartertone <492588+quartertone@users.noreply.github.com>
2024-07-25 13:39:17 -0700

    fixing darkmode LocalCookie again
    and this time I mean it

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,12 +1,16 @@
-  if (
-    localCookie("darkmode") == "true" ||
-  ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) && 
-  typeof defaultlight === "undefined" ) ) {
-    // dark mode
-    // document.querySelector("#darkmode").checked = true;
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else { 
-    // document.querySelector("#darkmode").checked = false;
-    document.documentElement.setAttribute("data-theme", "light");
+if (localCookie("darkmode") == "false") {
+  // document.querySelector("#darkmode").checked = false;
+  document.documentElement.setAttribute("data-theme", "light");
 
-  }
+} else if (
+  localCookie("darkmode") == "true" ||
+  ((window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) &&
+    typeof defaultlight === "undefined")) {
+  // dark mode
+  // document.querySelector("#darkmode").checked = true;
+  document.documentElement.setAttribute("data-theme", "dark");
+} else {
+  // document.querySelector("#darkmode").checked = false;
+  document.documentElement.setAttribute("data-theme", "light");
+
+}
